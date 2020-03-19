679da4c8e1c417929fee25430e609386b75b4bdf
Himanshu Bhatnagar <33115688+Himan10@users.noreply.github.com>
2020-03-19 23:38:36 +0530

    Edit extract_contents()
    now it only saves regex groups (if any find) :
    1. Quoted message
    2. The actual message
    instead of writing the entire fetched raw_message into a file.

--- fetch_mail.py
+++ fetch_mail.py
@@ -6,14 +6,16 @@ Requirements :
     stored inside a file or variable).
 """
 
-import imaplib
-import logging
-#import base64
+import imaplib  # For reading messages
+import logging  # For catching logs
+
+# import base64
 import os
 import email
-import socket                                   # Require for imaplib. connection
-from dotenv import load_dotenv                  # Require for loading env. variables
-from getpass import getpass
+import re
+import socket  # Require for imaplib. connection
+from dotenv import load_dotenv  # Require for loading env. variables
+from getpass import getpass  # For username/password
 
 logging.basicConfig(
     filename="logs",
@@ -54,29 +56,29 @@ class LPForum:
         typ, msgnums = obj.search(
             "utf-8", "FROM", "linkinpark@discoursemail.com"
         )  # Search message inside mailbox
-        if typ == 'OK':
+        if typ == "OK":
             return msgnums
         return 0
 
     def fetch_raw_message(self, obj, message_id):
-        """ Extracting message from message id's.
+        """ Extracting raw message from message id's.
         message_id contains id's of email messages
         by linkinpark@discoursemail """
 
         id_ = message_id[0].split()
         latest_email = id_[-1]  # Last id is the latest
-        typ, data = obj.fetch(latest_email, '(RFC822)')
+        typ, data = obj.fetch(latest_email, "(RFC822)")
 
         # Convert the fetched binary obj. into a string
-        raw_str = data[0][1]#.decode('utf-8')
+        raw_str = data[0][1]  # .decode('utf-8')
 
-        if typ == 'OK':
+        if typ == "OK":
             # raw_text contains two part -> id[0][0] and message body[0][1]
             return raw_str
         else:
             return False
 
-    def extract_contents(self, raw_string):
+    def extract_contents(self, user_tag, raw_string):
         """ Parse the message :
         header  (recipient information)
         payload (content) """
@@ -84,38 +86,51 @@ class LPForum:
         # Get Headers
         headers = {}
         message = email.message_from_bytes(raw_string)
-        print(' Is Multipart : ', message.is_multipart())
-        headers['To'] = message['To']
-        headers['From'] = message['From']
-        headers['Subject'] = message['Subject']
+        print(" Is Multipart : ", message.is_multipart())
+        headers["To"] = message["To"]
+        headers["From"] = message["From"]
+        headers["Subject"] = message["Subject"]
+        headers["Reply-To"] = message["Reply-To"]
 
         # Get Payloads
-        payload = None
+        payload_body = None
         for part in message.walk():
-            if part.get_content_type() == 'text/plain':
-                payload = message.get_payload()[0]
+            if part.get_content_type() == "text/plain":
+                payload_body = message.get_payload()[0]
 
-        payload = payload.get_payload()
-        with open('message', 'w') as f:
-            f.write(payload)
-        print(headers)
+        payload = payload_body.get_payload()
+
+        # Extract Useful message from a payload
+        pattern = r"(\[[\W\w]{9}user_tag.+?(?=\]).+?(?=\[\W\w{5}\]))(.*?(?=\[[\w]{5}))"
+        pattern = pattern.replace("user_tag", user_tag)
+        payload = repr(payload)
+        found = re.search(pattern, payload)
+
+        if found.group(1) and found.group(2) is not None:
+            # Write message/headers into a file
+            with open("message", "w") as f:
+                for key, value in headers.items():
+                    f.write(f"{key} : {value}\n\n")
+                f.write(f" Quote: {found.group(1)} \n \nMessage: {found.group(2)}")
+        else:
+            print(False)
 
 
 def main():
     """ Testing LPForum Class """
-    #username = input(" Enter Mail(without Domain) : ")
-    #password = getpass(" Enter Password : ")
+    # username = input(" Enter Mail(without Domain) : ")
+    # password = getpass(" Enter Password : ")
     load_dotenv()
     # Load username/password from environment variables
-    username = os.getenv('USERNAME')
-    password = os.getenv('PASSWORD')
+    username = os.getenv("USERNAME")
+    password = os.getenv("PASSWORD")
+    user_tag = str(input(" Enter your user tag : "))
 
     # Calling LPForum
     lpclient = LPForum(username, password)
     imap_obj = lpclient.login()
     msg_id = lpclient.get_message_id(imap_obj)
     r_msg = lpclient.fetch_raw_message(imap_obj, msg_id)
-    a = lpclient.extract_contents(r_msg)
+    lpclient.extract_contents(user_tag, r_msg)
     imap_obj.close()
     imap_obj.logout()
-    return a
