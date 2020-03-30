6f80b247266348531ec062ae02286f50bb074aec
Himanshu Bhatnagar <33115688+Himan10@users.noreply.github.com>
2020-03-30 23:48:09 +0530

    Edit message_body()
    1. removed the use of regex
    2. Import email.mime classes

--- send_mail.py
+++ send_mail.py
@@ -7,13 +7,17 @@ Method 1:
         -> Establishing connection
         -> Send mail()
         -> Quit()
+
 """
 
 import smtplib
-import ssl # for secure connection
+import ssl  # for secure connection
 import logging
 from dotenv import load_dotenv
-import re
+from email.message import EmailMessage
+from email.mime.multipart import MIMEMultipart # multipart = mixed
+from email.mime.text import MIMEText # multipart = text/plain
+from email.policy import default  # Make a use of RFC and \n For line ending
 from os import getenv
 
 logging.basicConfig(
@@ -47,25 +51,28 @@ class SendToLPForum:
         except Exception as err:
             logging.debug(err)
 
-    def message_body(self, server, To=None, Message=None):
-        """ Message body : payload and headers """
+    def message_body(self, server, Message=None):
+        """ Message body : payload and headers
+        Creating a Msg. object from scratch """
 
-        From = self.user
-        if To is None:
-            with open('message', 'r') as file:
-                To = file.readlines()[6]
+        data = []
+        msg = MIMEMultipart()
 
-            pattern = r'\<(.+)\>'
-            To = re.split(pattern, To)[-2]
-            To = To.replace('<', '')
-        print(To)
-        Message = """\
-        Subject: Hi Linkin Park
-        
-        I swear for the last time
-        I won't trust myself with you """
-        #server.connect(self.host, self.port)
-        server.sendmail(From, To, Message)
+        # Add headers
+        msg['From'] = self.user
+        with open('message', 'r') as fp:
+            data = fp.readlines()
+
+        msg['To'] = To = data[6].split(':')[1].strip()
+        msg['Subject'] = data[4].split(':')[1].strip()
+
+        # Attach a payload to the MIME msg object
+        if Message is None:
+            raise Exception('Emtpy Message')
+
+        msg.attach(MIMEText(Message, 'plain'))
+        Message = msg.as_string()
+        server.sendmail(self.user, To, Message)
 
 
 if __name__ == "__main__":
@@ -74,4 +81,13 @@ if __name__ == "__main__":
     password = getenv("PASSWORD")
     lp = SendToLPForum(username, password)
     server = lp.login()
-    lp.message_body(server)
+
+    # Get message content from File
+    lp_message = '''
+    Hey Man, how are you? Doing good?
+    Yeah, TBH i tried to make a hybrid theory soldier once but it's
+    too damn hard man.. i mean, whoever did this deserves a huge respect.
+    and let's see coz my after doing this Dealpool/Taskmaster pencil drawing,
+    i'll try to draw The Hunting Party poster. Cya man.. Take care and
+    good wishes to all of the LP family members '''
+    msg = lp.message_body(server, lp_message)
