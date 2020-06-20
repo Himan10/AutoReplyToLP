879018c8e07422f7405c3fb2005880a27a6db61c
Himanshu Bhatnagar <33115688+Himan10@users.noreply.github.com>
2020-06-20 16:59:25 +0530

    Added while loop
    This while loop is used to run the command or script for the infinite time until someone kills or terminate it.

--- interceptNotification.sh
+++ interceptNotification.sh
@@ -1,3 +1,6 @@
-#dbus-monitor --session "interface=org.freedesktop.Notifications" | grep --line-buffered "string" > ~/python/pyproject/LPforum/Dbus/temp.txt
+#Output stored in -> ~/python/pyproject/LPforum/Dbus/temp.txt
 # Directly check and Do shitty things at the time when it capture the notification
-dbus-monitor --session "interface='org.freedesktop.Notifications'" | grep --line-buffered "string" | python /home/hi-man/python/pyproject/LPforum/Dbus/formatNotification.py
+while True:
+do
+  dbus-monitor --session "interface='org.freedesktop.Notifications'" | grep --line-buffered "string" | python /home/hi-man/python/pyproject/LPforum/Dbus/formatNotification.py
+done
