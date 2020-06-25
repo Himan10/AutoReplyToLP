7b44de1752e33b4a995474dd0f185854ccc7f2d1
Himanshu Bhatnagar <33115688+Himan10@users.noreply.github.com>
2020-06-25 23:59:03 +0530

    run a py script once a notification is captured

--- formatNotification.py
+++ formatNotification.py
@@ -1,8 +1,20 @@
 #!/bin/env python
 
 # this while loop is for not considering two string fields which you get right after starting a process
-# ['string "ShanonPark posted in "HEY! YOU! What are you up to right now! (Part 3)" - Linkin Park / LPU Forums"'] 2 
+# Test string ->['string "ShanonPark posted in "HEY! YOU! What are you up to right now! (Part 3)" - Linkin Park / LPU Forums"'] 2 
+
 import sys
+import time
+import notify2
+from fetch_mail import main
+
+def Notify():
+    notify2.init('LPforum')
+    pop_up = notify2.Notification('Python Script initialized', 'Starts under 11 minutes')    
+    pop_up.set_timeout(12000)
+    pop_up.set_urgency(0)
+    pop_up.show()
+
 
 def fuckString(data: str) -> str:
     return data[0].split('"', 1)[1].replace('"', '')
@@ -16,8 +28,9 @@ del tempInput, wasteLines
 
 # string fields after above two non-considering fields
 DataNumber = 0
+FromLP = False
 matchedApp = None
-applications = ['notify-send', 'discord', "KDE Connect", "Brave", "Firefox",  "firefox"]
+applications = ['notify-send', "Brave", "Firefox",  "firefox"]
 constructData = []
 
 while True:
@@ -35,8 +48,9 @@ while True:
                 if DataNumber == 2:
                     constructData = [fuckString(NotifyData)]
                 if DataNumber == 3 and 'linkin park' in constructData[0].lower():
-                    with open('temp.txt', 'a') as file:
-                        file.write(f'{constructData[0]} : {NotifyData[0].split("string")[1]}\n')
+                    Notify() # notify the user about upcoming task
+                    time.sleep(10)
+                    main() # Run the script fetch_mail 
 
                 DataNumber += 1
     except KeyboardInterrupt as e:
