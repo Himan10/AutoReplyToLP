df995ccf95e78ea27159fe20bdc622103cedca4f
Himan10 <h3c.initiative46@gmail.com>
2020-07-01 19:41:54 +0530

    remove comment

--- intercept_notification.sh
+++ intercept_notification.sh
@@ -1,4 +1,3 @@
 #if i have to store the summary and data of notification  bubble then add a print() where i've called the timer.
-#Output stored in -> AutoReplyToLP/temp.txt
 #Directly check and Do shitty things at the time when it capture the notification
 dbus-monitor --session "interface='org.freedesktop.Notifications'" | grep --line-buffered "string" | python format_notification.py
