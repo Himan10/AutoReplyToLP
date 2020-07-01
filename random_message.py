c12adf7b48ac23edefe94ae0339cddb47bd400b8
Himan10 <h3c.initiative46@gmail.com>
2020-07-01 17:08:44 +0530

    format code

--- random_message.py
+++ random_message.py
@@ -5,31 +5,36 @@ lots of love @sender_tags
 
 from random import choice, sample
 
+
 def GenerateMessage():
 
-    fullMessage = ''
+    fullMessage = ""
 
     # Starting line
     startLine = ""
-    with open('txtFiles/greetings.txt', 'r') as file:
+    with open("txtFiles/greetings.txt", "r") as file:
         startLine = choice(file.readlines())
 
-    #Select a quote
+    # Select a quote
     FinalQuote = "Quote of the Day -> "
-    with open('txtFiles/quotes.txt', 'r') as file:
-        FinalQuote += choice(file.readlines())   # return a list
+    with open("txtFiles/quotes.txt", "r") as file:
+        FinalQuote += choice(file.readlines())  # return a list
 
-    #Create a line
+    # Create a line
     LastLine = ""
     names = []
-    with open('txtFiles/message2.txt', 'r') as file:
-        names = list(set(map(lambda x: '@'+x.split('-')[0].strip(), file.readlines())))
-    
-    if len(names) == 1:
-        LastLine += 'Yo Soldier, Keep up the good work '
-    else:
-        LastLine += 'Lots of love and virtual hugs to '
+    with open("txtFiles/message2.txt", "r") as file:
+        names = list(
+            set(map(lambda x: "@" + x.split("-")[0].strip(), file.readlines()))
+        )
 
-    fullMessage = startLine + FinalQuote + LastLine + ' '.join(names)
-    fullMessage = '\n\n'.join(sample(fullMessage.split('\n'), len(fullMessage.split('\n'))))
+    if len(names) == 1:
+        LastLine += "Yo Soldier, Keep up the good work "
+    else:
+        LastLine += "Lots of love and virtual hugs to "
+
+    fullMessage = startLine + FinalQuote + LastLine + " ".join(names)
+    fullMessage = "\n\n".join(
+        sample(fullMessage.split("\n"), len(fullMessage.split("\n")))
+    )
     return fullMessage
